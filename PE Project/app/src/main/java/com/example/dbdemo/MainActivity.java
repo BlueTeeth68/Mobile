8044c0b4247b57fdccceaf45bcdafa197ee294b9
Blue Teeth <daominhtri1000@gmail.com>
2023-07-09 12:25:23 +0700

    add simple UI

--- PE Project/app/src/main/java/com/example/dbdemo/MainActivity.java
+++ PE Project/app/src/main/java/com/example/dbdemo/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.dbdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private FloatingActionButton addButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        addButton = findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ PE Project/app/src/main/java/com/example/dbdemo/MyDBHelper.java
@@ -0,0 +1,42 @@
+package com.example.dbdemo;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyDBHelper extends SQLiteOpenHelper {
+
+    private Context context;
+    private static final String DATABASE_NAME = "BookLibrary.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String TABLE_NAME = "my_library";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_TITLE = "book_title";
+    private static final String COLUMN_AUTHOR = "book_author";
+    private static final String COLUMN_PAGES = "book_pages";
+
+    public MyDBHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+    String query = "CREATE TABLE " + TABLE_NAME +
+            " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            COLUMN_TITLE + " TEXT, " +
+            COLUMN_AUTHOR + " TEXT, " +
+            COLUMN_PAGES + " INTEGER);";
+    db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String query = "DROP TABLE IF EXISTS " + TABLE_NAME;
+        db.execSQL(query);
+        onCreate(db);
+    }
+}
